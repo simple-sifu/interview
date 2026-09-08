b69885b50560670365a00b35b55af3829bf3b1e6
Tommy Han <tommy.han.cs@gmail.com>
2026-09-08 19:41:34 -0400

    initial version

---
+++ Algos/Neetcode/TwoSum/TwoSum1.py
@@ -0,0 +1,52 @@
+# Given an array of integers nums and an integer target, 
+# return the indices i and j such that 
+# nums[i] + nums[j] == target and i != j.
+# You may assume that every input has exactly 
+# one pair of indices i and j that satisfy the condition.
+# Return the answer with the smaller index first.
+
+# Example 1:
+# Input: nums = [3,4,5,6], target = 7
+# Output: [0,1]
+# Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+# Example 2:
+# Input: nums = [4,5,6], target = 10
+# Output: [0,2]
+
+# Example 3:
+# Input: nums = [5,5], target = 10
+# Output: [0,1]
+
+
+# Constraints:
+# 2 <= nums.length <= 1000
+# -10,000,000 <= nums[i] <= 10,000,000
+# -10,000,000 <= target <= 10,000,000
+# Only one valid answer exists.
+
+def TwoSum( numbers: list[int], target: int):
+
+    rowOfLists = [None] * len(numbers)
+    if (len(numbers) == 2):
+        return f'[ %i, %i ]' % (0, 1)
+    for i, num in enumerate(numbers):
+        rowOfLists[i] = numbers[i:]
+    for x, listX in enumerate(rowOfLists):
+        for t, value in enumerate[int](listX):
+            firstNum = listX[0]
+            if (firstNum + value == target):
+                return f'[ %i, %i ]' % (x, t)
+
+
+nums = [3,4,5,6]
+target = 7
+print('TwoSum =', TwoSum(nums, target))
+
+nums = [4,5,6]
+target = 10
+print('TwoSum =', TwoSum(nums, target))
+
+nums = [5,5]
+target = 10
+print('TwoSum =', TwoSum(nums, target))
