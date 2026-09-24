b1bf165f5a709139def605ea3346a6e13fe038f0
Tommy Han <tommy.han.cs@gmail.com>
2026-09-24 13:48:38 -0400

    solve for topK

--- Algos/Neetcode/TopKFrequentElements/topKFrequentElements.py
+++ Algos/Neetcode/TopKFrequentElements/topKFrequentElements.py
@@ -5,22 +5,46 @@
 
 def topKFrequent(nums: list[int], k: int) -> list[int]:
 
+    print( '*' * 20)
+    counts = {}
+
+    for num in nums:
+        counts[num] = 1 + counts.get(num, 0) 
+    print("counts =", counts)
+
+    freq = [[] for _ in range(len(nums) + 1)]
+    print("freq before loop:", freq)
+    for num, count in counts.items():
+        print("inside loop num=", num, ", count=", count)
+        freq[count].append(num)
+        print("freq after loop:", freq)
+
+    print("freq after loop:", freq)
+    result = []
+    for index in range(len(freq)-1, 0, -1):
+        if len(result) >= k:
+            break
+        if freq[index]:
+            result.append(freq[index][0])
+
+    return result
+        
+
+
+    
 
 # Ex 1
-# Input: 
-    nums = [1,2,2,3,3,3], k = 2
+# Input: nums = [1,2,2,3,3,3,4], k = 2
 # Output: [2,3]
-
+print(topKFrequent([1,2,2,3,3,3,4], 2))
 
 
 # Ex 2
-# Input:  
-    nums = [7,7], k = 1
+# Input: nums = [7,7], k = 1
 # Output: [7]
+print(topKFrequent([7,7], 1))
 
 # Constraints
 # 1 <= nums.length <= 10^4.
 # -1000 <= nums[i] <= 1000
 # 1 <= k <= number of distinct elements in nums
-
-
